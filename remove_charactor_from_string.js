00b974a571d597f747452609b6a224b6a49ec81f
@Mohammad-ssgit <118876723+Mohammad-Nasri-Developer@users.noreply.github.com>
2024-01-14 09:43:10 +0400

    Create remove_charactor_from_string.js

---
+++ remove_charactor_from_string.js
@@ -0,0 +1,27 @@
+// function removeCharacterFromString(word, characterToRemove) {
+//   ///------- Solution 1
+//
+//   let letters = word.split("");
+//   let newString = "";
+//   for (let index = 0; index < letters.length; index++) {
+//     if (
+//       letters[index] !== characterToRemove.toLowerCase() &&
+//       letters[index] !== characterToRemove.toUpperCase()
+//     ) {
+//       newString = newString + letters[index];
+//     }
+//   }
+//
+//   return newString;
+// }
+
+///------- Solution 2
+
+function removeCharacterFromString(word, characterToRemove) {
+  return word.split("").filter((x) => x !== characterToRemove.toLowerCase() && x !== characterToRemove.toUpperCase() )
+      .join('');
+}
+
+console.log(removeCharacterFromString("ElddzeroD WebDD ddSchool", "d"));
+console.log(removeCharacterFromString("ElxzeroX Web Sxchool", "x"));
+console.log(removeCharacterFromString("Elzero@ Web@@ @@School", "@"));

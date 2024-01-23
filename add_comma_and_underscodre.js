767fec87e7e7e881e9479c3ac3bc157c6ccda8d2
@Mohammad-ssgit <118876723+Mohammad-Nasri-Developer@users.noreply.github.com>
2024-01-23 14:15:20 +0400

    add comma

--- add_comma_and_underscodre.js
+++ add_comma_and_underscodre.js
@@ -1,27 +1,59 @@
-function addCommasAndUnderscore(ginvenNumber) {
-  // 7 or 8 or 9 characters put underscore after third number from the right, and comma thfer the 6th from the right
-  // 4 or 5 or 6 characters put underscore after third number from the right
-  // 3 characters do not put anything.
+// My Solution
 
-  let nums = ginvenNumber.toLocaleString();
+// function addCommasAndUnderscore(ginvenNumber) {
+//   // 7 or 8 or 9 characters put underscore after third number from the right, and comma thfer the 6th from the right
+//   // 4 or 5 or 6 characters put underscore after third number from the right
+//   // 3 characters do not put anything.
 
-  return nums;
-  for (i = 0; i < nums.length; i++) {
-    //3
-    console.log("-----");
-    // console.log(nums);
+//   let result = ginvenNumber.toLocaleString();
 
-    if (nums.length <= 3) {
-      return nums[i];
-    }
+//   // convert To Array
+//   result = result.split("");
 
-    // 4,5,6
-    // 7,8,9
-  }
+//   // return result;s
+//   if (result.length <= 3) {
+//     let number = result.join("");
+//     return number;
+//   }
+
+//   /////////////
+//   if (result.length > 3 && result.length < 7) {
+//     //3
+//     console.log("--3<hhh");
+//     console.log(result);
+
+//     result.splice(1, 1, "_");
+
+//     return result.join("");
+//   }
+
+//   /////////////
+
+//   if (result.length > 3 && result.length >= 7) {
+//     // 7,8,9
+//     // result[result.length -4] = "_";
+//     result.splice(result.length - 4, 1, "_");
+
+//     return result.join("");
+//   }
+// }
+
+// OSAMA Solution
+function addCommasAndUnderscore(num) {
+  // Convert To String
+  let result = num.toLocaleString(); // Try Another Solution With RegExp
+  // Convert To Array
+  result = result.split("");
+  // Update The Last Comma Value
+  result[result.length - 4] = "_";
+  // Convert To String
+  result = result.join("");
+  return result;
 }
+// Try Another Solution With RegExp
 
-// console.log(addCommasAndUnderscore(120)); // 120
-// console.log(addCommasAndUnderscore(1530)); // 1_530
+console.log(addCommasAndUnderscore(120)); // 120
+console.log(addCommasAndUnderscore(1530)); // 1_530
 console.log(addCommasAndUnderscore(120510650)); // 120,510_650
-// console.log(addCommasAndUnderscore(510650480910)); // 510,650,780_910
-// console.log(addCommasAndUnderscore(12069057014032)); // 12,069,057,014_032
+console.log(addCommasAndUnderscore(510650480910)); // 510,650,780_910
+console.log(addCommasAndUnderscore(12069057014032)); // 12,069,057,014_032

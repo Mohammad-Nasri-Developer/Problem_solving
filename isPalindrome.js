56bc929951f1bf78d4c2854acccea98086d0e838
@Mohammad-ssgit <118876723+Mohammad-Nasri-Developer@users.noreply.github.com>
2024-06-19 16:50:49 +0400

    IS PALIDROME

---
+++ isPalindrome.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+// var isPalindrome_ApproachOne = function (x) {
+//     if (x < 0) {
+//         console.log("false");
+//         return false;
+//     }
+//     var sliced = x.toString().split("").reverse().join('');
+//     var parced = parseInt(sliced);
+//     if (parced === x) {
+//         console.log("true");
+//         return true;
+//     }
+//     console.log("false");
+//     return false;
+// };
+
+var isPalindrome_ApproachTwo = function (x) {
+
+    if (x < 0 || (x % 10 === 0 && x !== 0)) {
+        return false;
+    }
+    let reversed = 0;
+}
+
+isPalindrome_ApproachTwo(122)
+
+// isPalindrome_ApproachOne(121);
+// isPalindrome_ApproachOne(-121);
+isPalindrome_ApproachOne(10);

22349813e59a28c1ac83e061a3189321995f3380
Mohammad Rabiee Nasri <76831461+mohammad-nasri@users.noreply.github.com>
2024-12-20 10:45:07 +0400

    Create Number_of_Island.js

---
+++ Number_of_Island.js
@@ -0,0 +1,20 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+function numIslands(grid) {
+    // Edge Case : 0
+    if (!grid || grid.length === 0) return 0;
+
+    const m = grid.length; // rows count
+    const n = grid[0].length; // columns count
+    let numIslands = 0;
+
+    //breadth-first search : the idea is take the island and turn it into water
+
+
+    // loop on the grid
+        
+
+    return numIslands;
+}
